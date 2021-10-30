8c9003841ee9410cc6c03788006ae545eb973d86
Tanishq Gandhi <56472134+tanishqgandhi1908@users.noreply.github.com>
2021-10-30 15:40:30 +0530

    Full website

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("mynavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
